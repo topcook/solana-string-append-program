3710e224a377b2232456bc0baf83e42ef8209a9d
topcook <codecooker1996@gmail.com>
2022-05-20 16:10:39 +0200

    built and tested in localhost

---
+++ tests/mysolanaapp.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
+
+describe("mysolanaapp", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Mysolanaapp;
+
+  it("Is initializes the account", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
+    await program.methods.initialize('Hello world')
+    .accounts({
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    })
+    .signers([baseAccount])
+    .rpc();
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Data: ', account.data);
+    assert.ok(account.data === 'Hello world');
+    _baseAccount = baseAccount;
+  });
+
+  it('Updates a previously created account', async () => {
+    const baseAccount = _baseAccount;
+    await program.methods.update('Some new data')
+    .accounts({
+      baseAccount: baseAccount.publicKey,
+    })
+    .rpc();
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("Update data: ", account.data);
+    assert.ok(account.data === 'Some new data');
+    console.log("all account data: ", account);
+    console.log('All data: ', account.dataList);
+    assert.ok(account.dataList.length === 2);
+  });
+});
